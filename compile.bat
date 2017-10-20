daf7cc4015a38a2c49099ad8849f9ed4b7382b6e
Casper <cmvanb@gmail.com>
2017-10-20 16:08:42 +0200

    add FPS counter, add execute batch script

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @echo off
 
-set "DIV_INSTALL_DIR=C:\Projects\DIV\div-beta13-win\"
+set "DIV_INSTALL_DIR=C:\Projects\DIV\div-beta-nightly\"
 
 call %DIV_INSTALL_DIR%\system\div-WINDOWS.exe -c faust2.prg
---
+++ execute.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set "DIV_INSTALL_DIR=C:\Projects\DIV\div-beta-nightly\"
+
+call %DIV_INSTALL_DIR%\system\divdbg-WINDOWS.exe %DIV_INSTALL_DIR%\system\EXEC.EXE
+
