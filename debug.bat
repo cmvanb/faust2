afd0d588cd975a1b4577505c9fce09a6610b0598
Casper <cmvanb@gmail.com>
2017-10-30 00:21:38 +0100

    fix debug.bat not running the compiler

--- debug.bat
+++ debug.bat
@@ -10,16 +10,14 @@ set "PROGRAM_DIR=%~dp0"
 :: Change to DIV directory and run compiler on .PRG file.
 cd %DIV_INSTALL_DIR%
 echo Compiling...
-call system\div-WINDOWS.exe -c %PROGRAM_DIR%faust2.prg
+call system\div-WINDOWS.exe -c "%PROGRAM_DIR%faust2.prg"
 
 :: Change to program directory and copy over exec.path.
 :: TODO: This batch script should automatically create and populate exec.path so you don't have to bother with configuring it.
-cd %PROGRAM_DIR%
 echo Copying exec.path...
 xcopy /y "%PROGRAM_DIR%exec.path" "%DIV_INSTALL_DIR%system\"
 
 :: Change to DIV directory and run debugger on the outputted executable.
-cd %DIV_INSTALL_DIR%
 echo Running executable in debug mode...
 call system\divdbg-WINDOWS.exe system\EXEC.EXE
 
