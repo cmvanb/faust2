d88685e9caa40f203a1d9d585beff27fc74ee825
Casper <cmvanb@gmail.com>
2017-11-07 23:39:49 +0100

    fighting with batch files... copying exec.path works, generating it does not. Also, compilation works fine on desktop machine, not on laptop.

--- compile.bat
+++ compile.bat
@@ -6,19 +6,24 @@ set "DIV_INSTALL_DIR=C:\Projects\DIV\div-beta-nightly\"
 
 :: Remember program directory.
 set "PROGRAM_DIR=%~dp0"
+set "PROGRAM_FILE=faust2.prg"
 
 :: Change to DIV directory and run compiler on .PRG file.
 cd %DIV_INSTALL_DIR%
 echo Compiling...
-echo system\div-WINDOWS.exe -c "%PROGRAM_DIR%faust2.prg"
-call system\div-WINDOWS.exe -c "%PROGRAM_DIR%faust2.prg"
+echo system\div-WINDOWS.exe -c "%PROGRAM_DIR%%PROGRAM_FILE%"
+call system\div-WINDOWS.exe -c "%PROGRAM_DIR%%PROGRAM_FILE%"
 
-:: TODO: This batch script should automatically create and populate exec.path so you don't have to bother with configuring it.
 :: Copy over exec.path, this is needed for the DIV compiler to know where the program's assets are located.
-echo Copying exec.path...
-xcopy /y "%PROGRAM_DIR%exec.path" "%DIV_INSTALL_DIR%system\"
+xcopy /y "%PROGRAM_DIR%exec.path" system\
+
+:: NOTE: DOESN'T ACTUALLY WORK. ONLY COPYING OVER DIV3-GENERATED exec.path WORKS.
+:: Write the program directory to exec.path.
+::echo Writing %PROGRAM_DIR:~0,-1% to %DIV_INSTALL_DIR%system\exec.path...
+::echo %PROGRAM_DIR:~0,-1%> system\exec.path
 
 :: Change to program directory.
 echo Finished compiling.
 cd %PROGRAM_DIR%
+
 
